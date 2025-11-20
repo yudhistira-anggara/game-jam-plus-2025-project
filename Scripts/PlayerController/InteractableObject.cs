969e9cad95033f1ba64c25afe3c13546331515e3
SirVielAlot <150117364+SirVielAlot@users.noreply.github.com>
2025-11-20 18:03:11 +0700

    feat: Added InteractableObject and IInteractable interface for player interactions
     Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   Resources/Sprites/Character_675.png
    	new file:   Resources/Sprites/E_Button.png
    	new file:   Resources/Sprites/E_Button.png.import
    	modified:   Scenes/Tests/PlayerControllerTest.tscn
    	modified:   Scripts/Dialogue/DialoguePanel.cs
    	new file:   Scripts/PlayerController/IInteractable.cs
    	new file:   Scripts/PlayerController/IInteractable.cs.uid
    	new file:   Scripts/PlayerController/InteractableObject.cs
    	new file:   Scripts/PlayerController/InteractableObject.cs.uid
    	modified:   Scripts/PlayerController/PlayerController.cs
    	new file:   Scripts/Shaders/OutlineShader.gdshader
    	new file:   Scripts/Shaders/OutlineShader.gdshader.uid
    	modified:   icon.svg.import
    	modified:   project.godot

---
+++ Scripts/PlayerController/InteractableObject.cs
@@ -0,0 +1,76 @@
+using Godot;
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+public partial class InteractableObject : Node3D, IInteractable
+{
+	public AnimatedSprite3D ButtonSprite {get; private set;}
+	public bool CanInteract { get; private set; } = false;
+	private void _ready()
+	{
+		
+		ButtonSprite = GetNode<AnimatedSprite3D>("AnimatedSprite3D");
+		ButtonSprite.Visible = false;
+	}
+	/*
+	private void _physics_process(double delta)
+	{
+		if (Area3D.HasOverlappingBodies())
+		{
+			if (!InArea)
+			{
+				Entered();
+			}
+		}
+		else
+		{
+			if (InArea)
+			{
+				Exited();
+			}
+		}
+		if (InArea)
+		{
+			if (Input.IsActionPressed("interact",true))
+			{
+				if (!ButtonPressed)
+				{
+					ButtonPressed = true;
+					Interaction();
+				}
+			}
+			else
+			{
+				if (ButtonPressed)
+				{
+					ButtonPressed = false;
+					Entered();
+				}
+			}
+		}	
+	}*/
+	public void Entered()
+	{
+		ButtonSprite.Visible = true;
+		ButtonSprite.Play("Unpressed");
+		CanInteract = true;
+	}
+	public void Exited()
+	{
+		ButtonSprite.Visible = false;
+		CanInteract = false;
+	}
+	public void Interaction()
+	{
+		if (CanInteract)
+		{
+			ButtonSprite.Play("Pressed");
+		}
+	}
+	public void Uninteractable()
+	{
+		ButtonSprite.Play("Inactive");
+        CanInteract = false;
+	}
+}
