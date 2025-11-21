2e18445888a7f9ac594a7fd9830655bf3a721e61
Yudhistira Anggara Jayadinata <72541782+yuira34@users.noreply.github.com>
2025-11-22 07:11:12 +0800

    chore(ai): added ai object for state machine

--- Scripts/States/EnemyFollow.cs
+++ Scripts/States/EnemyFollow.cs
@@ -4,36 +4,36 @@ using System;
 [GlobalClass]
 public partial class EnemyFollow : State
 {
-    [Export] CharacterBody2D enemy;
-    [Export] double followSpeed = 20.0;
+	[Export] CharacterBody2D enemy;
+	[Export] double followSpeed = 20.0;
 
-    protected CharacterBody2D player;
+	protected CharacterBody2D player;
 
-    public override void Enter()
-    {
-        player = (CharacterBody2D) GetTree().GetFirstNodeInGroup("Player");
-    }
+	public override void Enter()
+	{
+		player = (CharacterBody2D) GetTree().GetFirstNodeInGroup("Player");
+	}
 
-    public override void Exit()
-    {
-        
-    }
+	public override void Exit()
+	{
+		
+	}
 
-    public override void Update(double delta)
-    {
-        
-    }
+	public override void Update(double delta)
+	{
+		
+	}
 
-    public override void PhysicsUpdate(double delta)
-    {
-        var direction = (player.GlobalPosition - enemy.GlobalPosition).Normalized();
+	public override void PhysicsUpdate(double delta)
+	{
+		var direction = (player.GlobalPosition - enemy.GlobalPosition).Normalized();
 
-        if (direction.Length() <= 20) return;
+		if (direction.Length() <= 20) return;
 
-        enemy.Velocity = direction * (float) followSpeed;
+		enemy.Velocity = direction * (float) followSpeed;
 
-        if (direction.Length() <= 60) return;
-        
-        EmitSignal(SignalName.Transitioned, "EnemyIdle");
-    }
+		if (direction.Length() <= 60) return;
+		
+		EmitSignal(SignalName.Transitioned, "EnemyIdle");
+	}
 }
