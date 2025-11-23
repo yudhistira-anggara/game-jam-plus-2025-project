99bb16f6a9ca02af078117792b4fa6841c47c2a6
v-Shekendo <elfretos1704@gmail.com>
2025-11-23 23:04:39 +0700

    autoload, audio manager

--- Scripts/UI/Tests/PanelShake.cs
+++ Scripts/UI/Tests/PanelShake.cs
@@ -3,14 +3,14 @@ using System;
 using System.Diagnostics;
 
 public partial class PanelShake : Node2D
-{  
+{
 	private FastNoiseLite _noise = new FastNoiseLite();
 	private Vector2 _originalPosition;
 	private CpuParticles2D _explosionParticles;
 	private void _ready()
 	{
 		_originalPosition = this.Position;
-		_explosionParticles = GetNode<CpuParticles2D>("Explosion"); 
+		_explosionParticles = GetNode<CpuParticles2D>("Explosion");
 	}
 	public void _on_button_mouse_entered()
 	{
@@ -18,6 +18,9 @@ public partial class PanelShake : Node2D
 	}
 	private void _shake_panel(float duration)
 	{
+		if (!IsInsideTree())
+			return;
+
 		var panel_tween = GetTree()?.CreateTween();
 		panel_tween.TweenMethod(
 			Callable.From<float>(_start_panel_shake), // Pass method reference
@@ -30,37 +33,42 @@ public partial class PanelShake : Node2D
 	private void _start_panel_shake(float intensity)
 	{
 		var panel_offset = _noise.GetNoise1D(Time.GetTicksMsec()) * intensity;
-		this.Position = new Vector2(_originalPosition.X + panel_offset,_originalPosition.Y);
+		this.Position = new Vector2(_originalPosition.X + panel_offset, _originalPosition.Y);
 	}
 	public void PlayExplosion()
 	{
+		if (!IsInsideTree())
+			return;
+
 		_shake_panel(2.0f);
-		_explosionParticles.OneShot = true; 
-		_explosionParticles.Emitting = true; 
+		_explosionParticles.OneShot = true;
+		_explosionParticles.Emitting = true;
 		_explosionParticles.Finished += OnExplosionFinished;
 		var panel_tween = GetTree().CreateTween();
 		panel_tween.TweenProperty(
 			this,
 			"scale",
-			new Vector2(1.0f,0.0f),
+			new Vector2(1.0f, 0.0f),
 			1.0f  // Duration
 		);
 	}
 	public void SpawnEffect()
 	{
+		if (!IsInsideTree())
+			return;
+
 		_shake_panel(0.3f);
-		this.Scale = new Vector2(0.0f,0.0f);
+		this.Scale = new Vector2(0.0f, 0.0f);
 		var panel_tween = GetTree()?.CreateTween();
 		panel_tween.TweenProperty(
 			this,
 			"scale",
-			new Vector2(1.0f,1.0f),
+			new Vector2(1.0f, 1.0f),
 			0.2f  // Duration
 		);
 	}
 	private void OnExplosionFinished()
 	{
-		
 		var parent = GetParent();
 		var grandParent = parent.GetParent();
 		grandParent.RemoveChild(parent);
---
+++ Scripts/Utilities/Singleton/AudioManager.cs
@@ -0,0 +1,115 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+namespace GameJam
+{
+    public partial class AudioManager : Node
+    {
+        public static AudioManager Instance { get; private set; }
+
+        private AudioStreamPlayer2D _musicPlayer;
+        private AudioStreamPlayer2D _audioPlayer;
+
+        private Dictionary<string, AudioStream> _loadedAudio = [];
+        private AudioStream _currentStream { get; set; }
+
+        public float GlobalVolume { get; set; } = 0;
+        private float MusicVolumeReal { get; set; } = -5;
+        public float MusicVolume { get; set; } = -5;
+        private float SFXVolumeReal { get; set; } = -5;
+        public float SFXVolume { get; set; } = -5;
+
+        public override void _Ready()
+        {
+            Instance = this;
+
+            _musicPlayer = new AudioStreamPlayer2D();
+            _audioPlayer = new AudioStreamPlayer2D();
+
+            _musicPlayer.VolumeDb = MusicVolumeReal;
+            _musicPlayer.Autoplay = true;
+
+            _audioPlayer.VolumeDb = SFXVolumeReal;
+            _audioPlayer.Autoplay = true;
+
+            AddChild(_musicPlayer);
+            AddChild(_audioPlayer);
+        }
+
+        public void PlayMusic(AudioStream soundEffect)
+        {
+            //
+        }
+
+        public void LoadSFX(string path)
+        {
+            if (_loadedAudio.ContainsKey(path))
+                return;
+
+            AudioStream audio = ResourceLoader.Load<AudioStream>(path);
+            if (audio != null)
+                _loadedAudio[path] = audio;
+            else
+                GD.PrintErr();
+        }
+
+        public void LoadAndPlaySFX(string path)
+        {
+            AudioStream audio;
+            if (_loadedAudio.TryGetValue(path, out AudioStream value))
+                audio = value;
+            else
+            {
+                audio = ResourceLoader.Load<AudioStream>(path);
+
+                if (audio != null)
+                    _loadedAudio[path] = audio;
+                else
+                    GD.PrintErr();
+            }
+
+            PlaySFX(audio);
+        }
+
+        public void PlaySFX(string name)
+        {
+            if (_loadedAudio.ContainsKey(name))
+            {
+                _audioPlayer.Stream = _loadedAudio[name];
+                _audioPlayer.Play();
+            }
+            else
+                LoadAndPlaySFX(name);
+        }
+
+        public void PlaySFX(AudioStream soundEffect)
+        {
+            _audioPlayer.Stream = soundEffect;
+            _audioPlayer.Play();
+        }
+
+        public void PauseResumeAudio(bool resume = true)
+        {
+            _audioPlayer.Stop();
+            _musicPlayer.Stop();
+        }
+
+        public void SetGlobalVolume(float volume)
+        {
+            GlobalVolume += volume;
+        }
+
+        public void SetSFXVolume(float volume)
+        {
+            SFXVolumeReal = GlobalVolume + SFXVolume;
+            SFXVolumeReal += volume;
+        }
+
+        public void SetMusicVolume(float volume)
+        {
+            MusicVolumeReal = GlobalVolume + MusicVolume;
+            SFXVolumeReal += volume;
+        }
+    }
+}
